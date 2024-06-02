25b9c47da268f170980f1841e90ef5426f7c807f
Jedliu <jedliu@gmail.com>
2024-06-02 21:44:03 +0800

    add mikro-orm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import { Module } from '@nestjs/common';
+import { MikroORM } from '@mikro-orm/core';
+import { MikroOrmMiddleware, MikroOrmModule } from '@mikro-orm/nestjs';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  OnModuleInit,
+} from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatGateway } from './chat.gateway';
+import * as config from './mikro-orm.config';
 import { UsersModule } from './users/users.module';
 
 const __dirname__ =
@@ -21,8 +29,19 @@ const __dirname__ =
       rootPath: join(__dirname__, '..', 'client'),
       exclude: ['/api/(.*)'],
     }),
+    MikroOrmModule.forRoot(config.default),
   ],
   controllers: [AppController],
   providers: [AppService, ChatGateway],
 })
-export class AppModule {}
+export class AppModule implements NestModule, OnModuleInit {
+  constructor(private readonly orm: MikroORM) {}
+
+  async onModuleInit(): Promise<void> {
+    if (process.env.NODE_ENV !== 'test') await this.orm.getMigrator().up();
+  }
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(MikroOrmMiddleware).forRoutes('*');
+  }
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import { v4 } from 'uuid';
+
+@Entity()
+export class User {
+  @PrimaryKey({ onCreate: () => v4() })
+  id!: string;
+
+  @Property()
+  fullName!: string;
+
+  @Property()
+  email!: string;
+
+  @Property()
+  password!: string;
+
+  @Property({ type: 'text' })
+  bio = '';
+}
